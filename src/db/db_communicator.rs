3b367bbb286151ec64999b128d3096b372d5a51d
RakuJa <danielegiachetto1999@gmail.com>
2023-11-14 01:56:11 +0100

    feat: implement traits and sources queries

--- src/db/db_communicator.rs
+++ src/db/db_communicator.rs
@@ -35,17 +35,29 @@ pub struct CreatureSource {
     name: String,
 }
 
-fn from_raw_vec_to_creature(raw_vec: Vec<RawCreature>) -> Vec<Creature> {
-    raw_vec.iter().map(from_raw_to_creature).collect()
+async fn from_raw_vec_to_creature(conn: &Pool<Sqlite>, raw_vec: Vec<RawCreature>) -> Vec<Creature> {
+    let mut creature_list = Vec::new();
+    for el in raw_vec {
+        creature_list.push(from_raw_to_creature(conn, &el).await);
+    }
+    creature_list
 }
 
-fn from_raw_to_creature(raw: &RawCreature) -> Creature {
+async fn from_raw_to_creature(conn: &Pool<Sqlite>, raw: &RawCreature) -> Creature {
     let creature_type =
         CreatureTypeEnum::from_str(raw.creature_type.clone().as_str()).unwrap_or_default();
     let alignment_enum = AlignmentEnum::from_str(raw.alignment.as_str()).unwrap_or_default();
     let size_enum = SizeEnum::from_str(raw.size.as_str()).unwrap_or_default();
     let rarity_enum = RarityEnum::from_str(raw.rarity.as_str()).unwrap_or_default();
     let archive_link = generate_archive_link(raw.aon_id, &creature_type);
+
+    let sources = sqlx::query_as::<_, CreatureSource>(
+        &*format!("SELECT * FROM source_table INTERSECT SELECT source_id FROM SOURCE_ASSOCIATION_TABLE WHERE creature_id == {}", raw.id)
+    ).fetch_all(conn).await.unwrap_or_default();
+
+    let traits = sqlx::query_as::<_, CreatureTrait>(
+        &*format!("SELECT * FROM trait_table INTERSECT SELECT trait_id FROM TRAIT_ASSOCIATION_TABLE WHERE creature_id == {}", raw.id)
+    ).fetch_all(conn).await.unwrap_or_default();
     Creature {
         id: raw.id,
         aon_id: raw.aon_id,
@@ -59,19 +71,19 @@ fn from_raw_to_creature(raw: &RawCreature) -> Creature {
         is_melee: raw.is_melee != 0,
         is_ranged: raw.is_ranged != 0,
         is_spell_caster: raw.is_spell_caster != 0,
-        sources: Vec::new(),
-        traits: Vec::new(),
+        sources: sources.into_iter().map(|curr_source| {curr_source.name}).collect(),
+        traits: traits.into_iter().map(|curr_trait| {curr_trait.name}).collect(),
         creature_type,
         archive_link,
     }
 }
 
 pub async fn fetch_creatures(conn: &Pool<Sqlite>) -> Result<Vec<Creature>, Error> {
-    let x = sqlx::query_as::<_, RawCreature>("SELECT * FROM CREATURE_TABLE")
+    let creatures = sqlx::query_as::<_, RawCreature>("SELECT * FROM CREATURE_TABLE")
         .fetch_all(conn)
         .await;
-    match x {
-        Ok(creature_list) => Ok(from_raw_vec_to_creature(creature_list)),
+    match creatures {
+        Ok(creature_list) => Ok(from_raw_vec_to_creature(conn,creature_list).await),
         Err(err) => {
             warn!("Error converting data from db {}", err);
             Err(err)
