cb0a29933d4e5701fba1d7f6fd4f5e0ffcdc8d97
rakuja <danielegiachetto1999@gmail.com>
2024-12-15 00:31:55 +0100

    chore: add overwrite db option

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # setup.py
-import os
+import pathlib
 import shutil
 import urllib.request
 import getopt, sys
@@ -10,14 +10,15 @@ from typing import Tuple
 argumentList = sys.argv[1:]
 
 # Options
-options = "hdc:"
+options = "hdco:"
 
 # Long options
-long_options = ["help", "db_version=", "copy_db"]
+long_options = ["help", "db_version=", "copy_db", "overwrite"]
 
-def handle_command_line_arguments() -> Tuple[str, bool]:
+def handle_command_line_arguments() -> Tuple[str, bool, bool]:
     db_version: str = "2.3.0" # Oldest DB version publicly available
     copy_db: bool = False
+    overwrite: bool = False
     try:
         # Parsing argument
         arguments, values = getopt.getopt(argumentList, options, long_options)
@@ -28,25 +29,32 @@ def handle_command_line_arguments() -> Tuple[str, bool]:
                 print("This script downloads or creates necessary file to build BYBE. \n"
                       "Should be executed the first time the project is built in the machine or when resetting the database \n"
                       "Pass the --db_version or -d argument to input a specific BYBE-DB version to download (>= 2.3.0) \n"
-                      "Use the --copy or -c argument to temporary copy the database (both downloaded or already present). \n"
+                      "Pass the --overwrite or -c argument to download even if a BYBE-DB is already present \n"
+                      "Pass the --copy or -c argument to temporary copy the database (both downloaded or already present). \n"
                       "This is used to enable Persistent startup (build with db copy and then delete the dirty db used to build)")
             elif currentArgument in ("-d", "--db_version"):
                 db_version = currentValue
             elif currentArgument in ("-c", "--copy_db"):
                 copy_db = True
+            elif currentArgument in ("-o", "--overwrite"):
+                overwrite = True
     except getopt.error:
         pass
-    return db_version, copy_db
+    return db_version, copy_db, overwrite
 
 def main():
     # Check if the file already exists or needs downloading
     x = handle_command_line_arguments()
     db_version: str = x[0]
     copy_enabled: bool = x[1]
+    overwrite: bool = x[2]
     print(f"Using DB version: {db_version}")
     remote_url: str = f"https://github.com/RakuJa/BYBE-DB/releases/download/v{db_version}/database.db"
     database_file: str = "database.db"
-    if not os.path.exists(database_file):
+    db_path = pathlib.Path(database_file)
+    if overwrite:
+        db_path.unlink(missing_ok=True)
+    if not db_path.exists():
         print("Downloading the database file...")
         urllib.request.urlretrieve(remote_url, database_file)
     else:
