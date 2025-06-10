44ed4cbb70f2ab358354bd27ca0c92fbb88b01e7
rakuja <danielegiachetto1999@gmail.com>
2025-06-10 14:30:33 +0200

    feat: handle complex resistances

--- src/db/data_providers/generic_fetcher.rs
+++ src/db/data_providers/generic_fetcher.rs
@@ -1,12 +1,6 @@
 use crate::models::item::weapon_struct::DamageData;
 use anyhow::Result;
-use itertools::Itertools;
-use sqlx::{FromRow, Pool, Sqlite};
-
-#[derive(FromRow)]
-pub struct MyString {
-    pub my_str: String,
-}
+use sqlx::{Pool, Sqlite};
 
 pub async fn fetch_unique_values_of_field(
     conn: &Pool<Sqlite>,
@@ -14,90 +8,69 @@ pub async fn fetch_unique_values_of_field(
     field: &str,
 ) -> Result<Vec<String>> {
     let query = format!(
-        "SELECT CAST(t1.{field} AS TEXT) AS my_str FROM ((SELECT DISTINCT ({field}) FROM {table})) t1"
+        "SELECT CAST(t1.{field} AS TEXT) FROM ((SELECT DISTINCT ({field}) FROM {table})) t1"
     );
-    let x: Vec<MyString> = sqlx::query_as(query.as_str()).fetch_all(conn).await?;
-    Ok(x.iter().map(|x| x.my_str.clone()).collect())
+    Ok(sqlx::query_scalar(query.as_str()).fetch_all(conn).await?)
 }
 
 pub async fn fetch_item_traits(conn: &Pool<Sqlite>, item_id: i64) -> Result<Vec<String>> {
-    Ok(sqlx::query_as!(
-        MyString,
-        "SELECT name AS my_str
+    Ok(sqlx::query_scalar!(
+        "SELECT name
          FROM TRAIT_TABLE INTERSECT
-         SELECT trait_id FROM TRAIT_ITEM_ASSOCIATION_TABLE WHERE item_id == ($1)",
+         SELECT trait_id FROM TRAIT_ITEM_ASSOCIATION_TABLE WHERE item_id == ($1)
+         ORDER BY name",
         item_id
     )
     .fetch_all(conn)
-    .await?
-    .into_iter()
-    .map(|x| x.my_str)
-    .sorted()
-    .collect())
+    .await?)
 }
 
 pub async fn fetch_weapon_traits(conn: &Pool<Sqlite>, weapon_id: i64) -> Result<Vec<String>> {
-    Ok(sqlx::query_as!(
-        MyString,
-        "SELECT name AS my_str
+    Ok(sqlx::query_scalar!(
+        "SELECT name
          FROM TRAIT_TABLE INTERSECT
-         SELECT trait_id FROM TRAIT_WEAPON_ASSOCIATION_TABLE WHERE weapon_id == ($1)",
+         SELECT trait_id FROM TRAIT_WEAPON_ASSOCIATION_TABLE WHERE weapon_id == ($1)
+         ORDER BY name",
         weapon_id
     )
     .fetch_all(conn)
-    .await?
-    .into_iter()
-    .map(|x| x.my_str)
-    .sorted()
-    .collect())
+    .await?)
 }
 
 pub async fn fetch_shield_traits(conn: &Pool<Sqlite>, shield_id: i64) -> Result<Vec<String>> {
-    Ok(sqlx::query_as!(
-        MyString,
-        "SELECT name AS my_str
+    Ok(sqlx::query_scalar!(
+        "SELECT name
          FROM TRAIT_TABLE INTERSECT
-         SELECT trait_id FROM TRAIT_SHIELD_ASSOCIATION_TABLE WHERE shield_id == ($1)",
+         SELECT trait_id FROM TRAIT_SHIELD_ASSOCIATION_TABLE WHERE shield_id == ($1)
+         ORDER BY name",
         shield_id
     )
     .fetch_all(conn)
-    .await?
-    .into_iter()
-    .map(|x| x.my_str)
-    .sorted()
-    .collect())
+    .await?)
 }
 
 pub async fn fetch_armor_traits(conn: &Pool<Sqlite>, armor_id: i64) -> Result<Vec<String>> {
-    Ok(sqlx::query_as!(
-        MyString,
-        "SELECT name AS my_str
+    Ok(sqlx::query_scalar!(
+        "SELECT name
          FROM TRAIT_TABLE INTERSECT
-         SELECT trait_id FROM TRAIT_ARMOR_ASSOCIATION_TABLE WHERE armor_id == ($1)",
+         SELECT trait_id FROM TRAIT_ARMOR_ASSOCIATION_TABLE WHERE armor_id == ($1)
+         ORDER BY name",
         armor_id
     )
     .fetch_all(conn)
-    .await?
-    .into_iter()
-    .map(|x| x.my_str)
-    .sorted()
-    .collect())
+    .await?)
 }
 
 pub async fn fetch_weapon_runes(conn: &Pool<Sqlite>, wp_id: i64) -> Result<Vec<String>> {
-    Ok(sqlx::query_as!(
-        MyString,
-        "SELECT name AS my_str
+    Ok(sqlx::query_scalar!(
+        "SELECT name
          FROM RUNE_TABLE INTERSECT
-         SELECT rune_id FROM RUNE_WEAPON_ASSOCIATION_TABLE WHERE weapon_id == ($1)",
+         SELECT rune_id FROM RUNE_WEAPON_ASSOCIATION_TABLE WHERE weapon_id == ($1)
+         ORDER BY name",
         wp_id
     )
     .fetch_all(conn)
-    .await?
-    .into_iter()
-    .map(|x| x.my_str)
-    .sorted()
-    .collect())
+    .await?)
 }
 
 pub async fn fetch_weapon_damage_data(conn: &Pool<Sqlite>, wp_id: i64) -> Result<Vec<DamageData>> {
@@ -113,17 +86,13 @@ pub async fn fetch_weapon_damage_data(conn: &Pool<Sqlite>, wp_id: i64) -> Result
 }
 
 pub async fn fetch_armor_runes(conn: &Pool<Sqlite>, wp_id: i64) -> Result<Vec<String>> {
-    Ok(sqlx::query_as!(
-        MyString,
-        "SELECT name AS my_str
+    Ok(sqlx::query_scalar!(
+        "SELECT name
          FROM RUNE_TABLE INTERSECT
-         SELECT rune_id FROM RUNE_ARMOR_ASSOCIATION_TABLE WHERE armor_id == ($1)",
+         SELECT rune_id FROM RUNE_ARMOR_ASSOCIATION_TABLE WHERE armor_id == ($1)
+         ORDER BY name",
         wp_id
     )
     .fetch_all(conn)
-    .await?
-    .into_iter()
-    .map(|x| x.my_str)
-    .sorted()
-    .collect())
+    .await?)
 }
--- src/models/db/raw_immunity.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-use sqlx::FromRow;
-
-#[derive(Serialize, Deserialize, FromRow)]
-pub struct RawImmunity {
-    pub name: String,
-}
